da3ecf26bd907431cac2e19618277b3eb5c8d095
Pubudu <pdodangoda@atlassian.com>
2024-02-16 15:26:07 +1100

    merge tool js

---
+++ merge-tool.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env zx
+
+import 'zx/globals'
+
+console.log(`
+##################
+MERGE TOOL RUNNING
+##################
+`);
+
+console.log("Changes in common parent", $.env);
+//
+// echo "Changes in base branch: $1"
+// echo "---------"
+// cat $1
+// echo ""
+//
+// echo "Changes from the branch: $2"
+// echo "---------"
+// cat $2
+// echo ""
+//
+// echo "Other: $3"
+// echo "---------"
+// cat $3
+// echo ""
+//
+// echo "Resolving conflict ..."
+// sleep 2
+// echo "# This is the merge result \n $2" > $2
+// echo "Conflict resolved!"
+//
+// echo "resolved: $2"
+// echo "---------"
+// cat $2
+// echo ""
+//
+// exit 0
